56fa50d1921500b52866431ac337da3d2b268a2d
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-09-29 21:36:01 -0600

    + test(user): do corrections and test it

--- src/app/search/user/user.component.ts
+++ src/app/search/user/user.component.ts
@@ -4,9 +4,9 @@ import { Item } from 'src/app/types/user.types';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
 })
 export class UserComponent {
   @Input()
-  user!: Item;
+  user: Item | Partial<Item> | undefined;
 }
--- tests/search/user/user.component.spec.ts
+++ tests/search/user/user.component.spec.ts
@@ -5,6 +5,7 @@ import { UserComponent } from '../../../src/app/search/user/user.component';
 describe('UserComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
+  let compiled: HTMLElement
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,9 +16,39 @@ describe('UserComponent', () => {
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should match snapshot', () => {
+    expect(compiled).toMatchSnapshot();
+  });
+
+  it('should render user', () => {
+    component.user = {
+      login: 'test',
+      id: 1,
+      avatar_url: 'test',
+      html_url: 'test',
+      score: 1,
+    };
+    fixture.detectChanges();
+    expect(compiled).toMatchSnapshot();
+  });
+
+  it('should open user on _blank', () => {
+    component.user = {
+      login: 'test',
+      id: 1,
+      avatar_url: 'test',
+      html_url: 'test',
+      score: 1,
+    };
+    fixture.detectChanges();
+    const a = compiled.querySelector('a');
+    expect(a?.target).toBe('_blank');
   });
 });
