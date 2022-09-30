e75dbb18b34fd5fdc42bbe4999fa7b537a7f8533
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-09-29 22:27:52 -0600

    + test(search): search users on submit

--- tests/search/search.component.spec.ts
+++ tests/search/search.component.spec.ts
@@ -1,9 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { GithubService } from '../../src/app/github/github.service';
 
 import { SearchComponent } from '../../src/app/search/search.component';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import { PaginatorComponent } from '../../src/app/search/paginator/paginator.component';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
@@ -12,19 +13,28 @@ describe('SearchComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SearchComponent],
+      declarations: [SearchComponent, PaginatorComponent],
       imports: [HttpClientTestingModule, ReactiveFormsModule],
-      providers: [GithubService],
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
-
-  //
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should match snapshot', () => {
+    expect(compiled).toMatchSnapshot();
+  });
+
+  it('should search users on submit', () => {
+    component.onSubmit();
+    expect(component.showSplash).toBeFalsy();
   });
 });
